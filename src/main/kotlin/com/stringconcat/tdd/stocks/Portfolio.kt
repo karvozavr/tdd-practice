3cd36177246ccb88e7d6b00400cdbf1e92718c84
Dmitrii Abramov <karvozavr@gmail.com>
2021-02-27 17:55:23 +0300

    Add portfolio with shares from different markets with different currencies

--- src/main/kotlin/com/stringconcat/tdd/stocks/Portfolio.kt
+++ src/main/kotlin/com/stringconcat/tdd/stocks/Portfolio.kt
@@ -1,6 +1,7 @@
 package com.stringconcat.tdd.stocks
 
 import com.stringconcat.tdd.Money
+import com.stringconcat.tdd.RateProvider
 import com.stringconcat.tdd.sumToCurrency
 import java.math.BigDecimal
 
@@ -8,17 +9,18 @@ class Portfolio(private val instruments: Map<Shares, Int> = emptyMap()) {
 
     fun totalPrice(
         inCurrency: Money.Currency,
-        marketInfoProvider: MarketInfoProvider
+        marketInfoProvider: MarketInfoProvider,
+        rateProvider: RateProvider
     ): Money {
         return instruments.asSequence()
-            .map { priceForInstrument(instrument = it.key, inPosession = it.value, marketInfoProvider) }
+            .map { priceForInstrument(instrument = it.key, inPossession = it.value, marketInfoProvider) }
             .toList()
-            .sumToCurrency(inCurrency, { BigDecimal.ZERO })
+            .sumToCurrency(inCurrency, rateProvider)
     }
 
     private fun priceForInstrument(
         instrument: Shares,
-        inPosession: Int,
+        inPossession: Int,
         marketInfoProvider: MarketInfoProvider
-    ) = marketInfoProvider.getInstrumentPrice(instrument) * inPosession
+    ) = marketInfoProvider.getInstrumentPrice(instrument) * inPossession
 }
--- src/test/kotlin/com/stringconcat/tdd/stocks/PortfolioTest.kt
+++ src/test/kotlin/com/stringconcat/tdd/stocks/PortfolioTest.kt
@@ -3,21 +3,41 @@ package com.stringconcat.tdd.stocks
 import com.stringconcat.tdd.Money
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 internal class PortfolioTest {
 
     private val TESLA_SHARES = Shares(name = "TSLA", market = TradingMarket.NASDAQ)
+    private val NOVARTIS_SHARES = Shares(name = "NOVN", market = TradingMarket.SWX)
 
     @Test
     fun `empty portfolio should have price of 0 USD`() {
         val portfolio = Portfolio()
-        portfolio.totalPrice(Money.Currency.USD, InMemoryMarketInfoProvider(emptyMap())) shouldBe Money.dollar(0)
+        val totalPrice =
+            portfolio.totalPrice(Money.Currency.USD, InMemoryMarketInfoProvider(emptyMap()), { BigDecimal.ZERO })
+        totalPrice shouldBe Money.dollar(0)
     }
 
     @Test
     fun `portfolio with one Tesla share valued 42 USD should cost 42 USD`() {
         val portfolio = Portfolio(mapOf(TESLA_SHARES to 1))
         val marketInfoProvider = InMemoryMarketInfoProvider(mapOf(TESLA_SHARES to Money(42, Money.Currency.USD)))
-        portfolio.totalPrice(Money.Currency.USD, marketInfoProvider) shouldBe Money.dollar(42)
+
+        val totalPrice = portfolio.totalPrice(Money.Currency.USD, marketInfoProvider, { BigDecimal.ZERO })
+        totalPrice shouldBe Money.dollar(42)
+    }
+
+    @Test
+    fun `portfolio with 2 Tesla shares valued 10 USD and 3 Novartis shares valued 3 CHF costs 49 CHF`() {
+        val portfolio = Portfolio(mapOf(TESLA_SHARES to 2, NOVARTIS_SHARES to 3))
+        val marketInfoProvider = InMemoryMarketInfoProvider(
+            mapOf(
+                TESLA_SHARES to Money(10, Money.Currency.USD),
+                NOVARTIS_SHARES to Money(3, Money.Currency.CHF)
+            )
+        )
+
+        val totalPrice = portfolio.totalPrice(Money.Currency.CHF, marketInfoProvider, { BigDecimal(2) })
+        totalPrice shouldBe Money.franc(49)
     }
 }
